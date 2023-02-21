94b5c41a5c4521cc046cd272a124923468838602
Priyanshi_Vasoya <priyanshivasoya009@gmail.com>
2023-02-21 18:16:45 +0530

    "todotask"

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const getLocalItmes = () => {
+  let list = localStorage.getItem('lists');
+  console.log(list);
+
+  if (list) {
+    return JSON.parse(localStorage.getItem('lists'));
+  } else {
+    return [];
+  }
+}
 
 const App = () => {
 
-  const [data, setData] = useState();
-  const [item, setItems] = useState([]);
-  const [edit, setEdit] = useState();
+  const [data, setData] = useState('');
+  const [item, setItems] = useState(getLocalItmes());
+  const [edit, setEdit] = useState(null);
   const [toggle, setToggle] = useState(true);
 
-  const additems = (id) => {
+  
+  const addItem = () => {
     if (!data) {
       alert("please fill the data");
     } else if (data && !toggle) {
-      item.map((elem) => {
-        if (elem.id === edit) {
-          return { ...item, name: data }
-        }
-        return item;
-      })
+      setItems(
+        item.map((elem) => {
+          if (elem.id === edit) {
+            return { ...elem, name: data }
+          }
+          return elem;
+        })
+      )
       setToggle(true);
 
-      setItems('');
+      setData('');
 
-      setEdit(false)
+      setEdit(null)
     }
     else {
-      const newdata = [id = newdata().setTime().toString(), [name] = data]
-      return [...newdata, setItems]
-      setItems('')
+      const newdata = { id: new Date().getTime().toString(), name: data }
+      setItems([...item, newdata]);
+      setData('')
     }
+
+    setData({})
   }
 
   const onDel = (index) => {
@@ -42,44 +58,43 @@ const App = () => {
     let EditData = item.find((elem) => {
       return elem.id === id
     });
-    console.log(edit);
+    console.log(EditData);
 
     setToggle(false);
 
-    setData(edit.name);
+    setData(EditData.name);
 
     setEdit(id);
 
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const name = e.target.name
-    const value = e.target.value
-    setItems('');
-  }
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(item))
+  }, [item]);
+
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <div>
         <input
           type="text"
           placeholder='add your todo'
-          name='data'
-          value={data.name}
-          onChange={additems} />
-        {toggle ? <button type='submit'>Update</button> : <button type='submit'>Add</button>}
-      </form>
+          value={data.tname}
+          onChange={(e) => setData(e.target.value)}
+        />
+        {toggle ? <button onClick={addItem}>Add</button> : <button onClick={addItem}>Update</button> }
+      </div>
       <div>
         {item.map((elem) => {
           return (
-            <>
-              <div>{data.name}</div>
+          
               <div key={elem.id}>
-                <button onClick={() => onEdit(elem.id)}>EDIT</button>
-                <button onClick={() => onDel(elem.id)}>DELETE</button>
-              </div>
-            </>
+                <h5>{elem.name}</h5>
+                <div>
+                  <button onClick={() => onEdit(elem.id)}>EDIT</button>
+                  <button onClick={() => onDel(elem.id)}>DELETE</button>
+                </div>
+              </div> 
           )
         })}
       </div>
